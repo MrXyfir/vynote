1aac4680bdb75c0267b0855d2fb6d59b0e68716a
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-02-05 10:43:31 -0800

    Added controller for retrieving user's account info

--- controllers/user/get.ts
+++ controllers/user/get.ts
@@ -1,1 +1,14 @@
-﻿
+﻿import db = require("../../lib/db");
+
+export = (socket: SocketIO.Socket, fn: Function) => {
+
+    db(cn => {
+        let sql: string = "SELECT email, config, subscription FROM users WHERE user_id = ?";
+        cn.query(sql, [socket.session.uid], (err, rows) => {
+            cn.release();
+
+            fn(!err && rows.length == 1 ? rows[0] : {});
+        });
+    });
+
+};
