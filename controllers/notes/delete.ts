9325d6d665eb8cbb79760fcd203fd625519e9bfa
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-02-07 09:37:00 -0800

    Added controller for deleting note elements

--- controllers/notes/delete.ts
+++ controllers/notes/delete.ts
@@ -1,1 +1,20 @@
-﻿
+﻿import db = require("../../lib/db");
+
+export = (socket: SocketIO.Socket, note: number, elements: number[], fn: Function) => {
+
+    let sql: string = "DELETE FROM note_elements WHERE note_id IN (?) AND doc_id IN "
+        + "(SELECT doc_id FROM documents WHERE doc_id = ? AND user_id = ?)";
+
+    db(cn => cn.query(sql, [elements.join(", "), note, socket.session.uid], (err, result) => {
+        cn.release();
+
+        if (err || !result.affectedRows) {
+            fn(true);
+        }
+        else {
+            fn(false);
+            socket.broadcast.to(note).emit("delete elements", elements);
+        }
+    }));
+
+};
