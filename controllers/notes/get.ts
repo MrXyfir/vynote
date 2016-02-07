1408025ed531146f6692a8852bf864e828ec1a29
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-02-07 08:26:09 -0800

    Added controller for retrieving all of a document's note elements

--- controllers/notes/get.ts
+++ controllers/notes/get.ts
@@ -1,1 +1,18 @@
-﻿
+﻿import db = require("../../lib/db");
+
+export = (socket: SocketIO.Socket, note: number, fn: Function) => {
+
+    db(cn => {
+        let sql: string = "SELECT parent_id, note_id, level, content, flags FROM note_elements "
+            + "WHERE doc_id IN (SELECT doc_id FROM documents WHERE doc_id = ? AND user_id = ?)";
+        cn.query(sql, [note, socket.session.uid], (err, rows) => {
+            cn.release();
+
+            if (err || !rows.length)
+                fn([]);
+            else
+                fn(rows);
+        });
+    });
+
+};
