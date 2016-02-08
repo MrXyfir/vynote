6578c9c97ac4b3652ce1a795f8bfe7c746ab88a5
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-02-08 15:32:33 -0800

    Added controller for updating a document's content

--- controllers/pages/update.ts
+++ controllers/pages/update.ts
@@ -1,1 +1,37 @@
-﻿
+﻿import db = require("../../lib/db");
+
+interface IData {
+    doc: number, content: string, append?: boolean
+}
+
+export = (socket: SocketIO.Socket, data: IData, fn: Function) => {
+
+    let sql: string = `
+        UPDATE document_content SET content = ${data.append ? "CONCAT(content, ?)" : "?"}
+        WHERE doc_id IN (
+            SELECT doc_id FROM documents WHERE doc_id = ? AND user_id = ?
+        )
+        OR doc_id IN (
+            SELECT doc_id FROM document_contributors WHERE doc_id = ? AND user_id = ? AND can_write = 1
+        )
+    `;
+
+    let vars = [
+        data.content,
+        data.doc, socket.session.uid,
+        data.doc, socket.session.uid
+    ];
+
+    db(cn => cn.query(sql, vars, (err, result) => {
+        cn.release();
+
+        if (err || !result.affectedRows) {
+            fn(true);
+        }
+        else {
+            fn(false);
+            socket.broadcast.to(''+data.doc).emit("update document", data);
+        }
+    }));
+
+};
