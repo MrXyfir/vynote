8486f5ff4e7fc6be72f223036f1a36b7ef380d3e
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-02-09 14:20:22 -0800

    Return shortcuts if user has a subscription

--- controllers/user/get.ts
+++ controllers/user/get.ts
@@ -5,9 +5,20 @@ export = (socket: SocketIO.Socket, fn: Function) => {
     db(cn => {
         let sql: string = "SELECT email, config, subscription FROM users WHERE user_id = ?";
         cn.query(sql, [socket.session.uid], (err, rows) => {
-            cn.release();
+            // Grab user's shortcuts if user has subscription
+            if (socket.session.subscription > Date.now()) {
+                sql = "SELECT name, content FROM shortcuts WHERE user_id = ?";
+                cn.query(sql, [socket.session.uid], (err, shortcuts) => {
+                    cn.release();
 
-            fn(!err && rows.length == 1 ? rows[0] : {});
+                    rows[0].shortcuts = shortcuts;
+                    fn(rows[0]);
+                });
+            }
+            else {
+                cn.release();
+                fn(rows[0]);
+            }
         });
     });
 
