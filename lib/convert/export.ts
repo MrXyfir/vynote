7d781dd104498b24bf9c5b7e5a391e8d4278a34a
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-02-11 10:53:23 -0800

    Added modules for converting note elements to a note object and documents to text files

---
+++ lib/convert/export.ts
@@ -0,0 +1,65 @@
+﻿import * as fs from "fs";
+
+import db = require("../db");
+import buildNoteObject = require("./note-elements");
+
+function writeToFile(file: string, content: string, fn: Function) {
+    fs.writeFile(file, content, (err) => {
+        content = null;
+        fn(err ? "" : file);
+    });
+}
+
+function convertNoteDocument(file: string, elements: any[], fn: Function) {
+    // Build note from elements
+    let note = buildNoteObject(elements);
+
+    // Convert object to text format
+    const renderText = (parent: number, level: number): string => {
+
+        let content: string = "";
+
+        // Grab content of all children under parent
+        note.ref[parent].forEach(child => {
+            for (var i = 0; i < level; i++)
+                content += "\t";
+
+            content += "- " + note.notes[child].content + "\r\n";
+
+            // Render text on child
+            content += renderText(child, level + 1);
+        });
+
+        return content;
+
+    };
+
+    let content: string = renderText(0, 0);
+    note = null;
+
+    writeToFile(file, content, fn);
+}
+
+export = (doc: number, docType: number, fn: Function) => {
+
+    let sql: string = "";
+
+    if (docType == 1)
+        sql = "SELECT note_id, parent_id, content FROM note_elements WHERE doc_id = ?";
+    else
+        sql = "SELECT content FROM document_content WHERE doc_id = ?";
+
+    db(cn => cn.query(sql, [doc], (err, rows) => {
+        cn.release();
+
+        let file: string = doc + "-" + Math.round(Date.now()) + ".txt";
+
+        if (err || !rows.length)
+            fn("");
+        else if (docType == 1)
+            convertNoteDocument(file, rows, (file: string) => fn(file));
+        else
+            writeToFile(file, rows[0].content, fn);
+    }));
+
+};
---
+++ lib/convert/note-elements.ts
@@ -0,0 +1,30 @@
+﻿interface INoteElement {
+    note_id: number, parent_id: number, content: string
+}
+
+export = (elements: INoteElement[]) => {
+    let note = {
+        ref: {
+            // id: [child: number, child2: number, ...]
+        },
+        notes: {
+            // id: { parent: number, content: string }
+        }
+    };
+
+    elements.forEach(element => {
+        // Add note's parent and content to note.notes object by ID
+        note.notes[element.note_id] = {
+            parent: element.parent_id, content: element.content
+        };
+
+        // Add note as child for element.parent_id in ref
+        if (note.ref[element.parent_id] === undefined)
+            note.ref[element.parent_id] = [element.note_id];
+        else
+            note.ref[element.parent_id].push(element.note_id);
+    });
+    elements = null;
+
+    return note;
+}
