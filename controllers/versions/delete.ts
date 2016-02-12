49cff2dea68fae0502df17394708292bf18e38b4
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-02-11 16:32:55 -0800

    Now checks to ensure user has access to document before deleting version

--- controllers/versions/delete.ts
+++ controllers/versions/delete.ts
@@ -2,9 +2,21 @@
 
 export = (socket: SocketIO.Socket, doc: number, name: string, fn: Function) => {
 
-    let sql: string = `DELETE FROM document_versions WHERE doc_id = ? AND name = ?`;
+    let sql: string = `
+        DELETE FROM document_versions WHERE doc_id IN (
+            SELECT doc_id FROM documents WHERE (doc_id = ? AND user_id = ?) 
+            OR (doc_id IN (
+                SELECT doc_id FROM document_contributors WHERE doc_id = ? AND user_id = ?
+            ))
+        ) AND name = ?
+    `;
+    let vars = [
+        doc, socket.session.uid,
+        doc, socket.session.uid,
+        name
+    ];
 
-    db(cn => cn.query(sql, [doc, name], (err, result) => {
+    db(cn => cn.query(sql, vars, (err, result) => {
         cn.release();
 
         fn(!!err || !result.affectedRows);
