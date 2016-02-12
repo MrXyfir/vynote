8feeb04d796b54a6242fb399d73cdaa151a25ac4
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-02-11 16:28:45 -0800

    Added controller for deleting a document version

---
+++ controllers/versions/delete.ts
@@ -0,0 +1,12 @@
+﻿import db = require("../../lib/db");
+
+export = (socket: SocketIO.Socket, doc: number, name: string, fn: Function) => {
+
+    let sql: string = `DELETE FROM document_versions WHERE doc_id = ? AND name = ?`;
+
+    db(cn => cn.query(sql, [doc, name], (err, result) => {
+        cn.release();
+
+        fn(!!err || !result.affectedRows);
+    }));
+};
