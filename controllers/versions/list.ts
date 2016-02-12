f0dc4d0995550d5f6b128fd9a8d939a3f4e6977f
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-02-11 16:36:17 -0800

    Added controller for listing all of a document's versions

---
+++ controllers/versions/list.ts
@@ -0,0 +1,23 @@
+﻿import db = require("../../lib/db");
+
+export = (socket: SocketIO.Socket, doc: number, name: string, fn: Function) => {
+
+    let sql: string = `
+        SELECT name, content FROM document_versions WHERE doc_id IN (
+            SELECT doc_id FROM documents WHERE (doc_id = ? AND user_id = ?) 
+            OR (doc_id IN (
+                SELECT doc_id FROM document_contributors WHERE doc_id = ? AND user_id = ?
+            ))
+        ) ORDER BY created DESC
+    `;
+    let vars = [
+        doc, socket.session.uid,
+        doc, socket.session.uid
+    ];
+
+    db(cn => cn.query(sql, vars, (err, rows) => {
+        cn.release();
+
+        fn(!!err || !rows.length ? [] : rows);
+    }));
+};
