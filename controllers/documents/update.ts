d2f2077fbf49ee3680ebc85501736f094443c406
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-02-14 12:51:47 -0800

    Added new update actions to decrease how often a complete overwrite is needed

--- controllers/documents/update.ts
+++ controllers/documents/update.ts
@@ -1,13 +1,49 @@
 ﻿import db = require("../../lib/db");
 
 interface IData {
-    doc: number, content: string, append?: boolean
+    doc: number, content: any, action: string
 }
 
 export = (socket: SocketIO.Socket, data: IData, fn: Function) => {
 
+    let value: string = "";
+
+    // Determine what to place after 'SET content = '
+    switch (data.action) {
+        case "OVERWRITE":
+            value = "?"; break;
+        case "APPEND":
+            value = "CONCAT(content, ?)"; break;
+        case "REMOVE":
+            value = "SUBSTR(content, 1, CHAR_LENGTH(content) - ?)"; break;
+        case "INSERT":
+            value = "INSERT(content, ?, CHAR_LENGTH(?), ?)"; break;
+        case "REPLACE":
+            value = "REPLACE(content, ?, ?)";
+    }
+
+    let vars = [];
+
+    // Determine vars based on action
+    switch (data.action) {
+        case "INSERT":
+            vars = [ data.content.at, data.content.length, data.content.string ];
+            break;
+        case "REPLACE":
+            vars = [ data.content.find, data.content.replace ];
+            break;
+        default:
+            vars = [ data.content ];
+    }
+
+    // Add variables that are always present
+    vars = vars.concat([
+        data.doc, socket.session.uid,
+        data.doc, socket.session.uid
+    ]);
+
     let sql: string = `
-        UPDATE document_content SET content = ${data.append ? "CONCAT(content, ?)" : "?"}
+        UPDATE document_content SET content = ${value} 
         WHERE doc_id IN (
             SELECT doc_id FROM documents WHERE doc_id = ? AND user_id = ?
         )
@@ -15,12 +51,6 @@ export = (socket: SocketIO.Socket, data: IData, fn: Function) => {
             SELECT doc_id FROM document_contributors WHERE doc_id = ? AND user_id = ? AND can_write = 1
         )
     `;
-
-    let vars = [
-        data.content,
-        data.doc, socket.session.uid,
-        data.doc, socket.session.uid
-    ];
 
     db(cn => cn.query(sql, vars, (err, result) => {
         cn.release();
