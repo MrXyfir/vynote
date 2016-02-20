b57da0f857f10879e96c90f629a504e2b261c7cf
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-02-20 14:48:22 -0800

    Added module for returning an array of {name, id} folder objects for parents of scoped folder

---
+++ client/lib/explorer/scope-parents.js
@@ -0,0 +1,25 @@
+export default function (folders, scope) {
+	const getParent = (id) => {
+		if (folders[id].parent_id === 0) {
+			return [{ name: folders[id].name, id }];
+		}
+		else {
+			return [{ name: folders[id].name, id }].concat(
+				getParent(folders[id].parent_id)
+			);
+		}
+	};
+	
+	if (scope === 0) {
+		return [ { name: "", id: 0 } ];
+	}
+	else {
+		let scopeParents = getParent(folders[scope].parent_id);
+		
+		// getParent array starts with current scope and ends with root
+		// reverse so scopeParents starts with root down to scope
+		scopeParents.reverse(); 
+		
+		return scopeParents; 
+	}
+}
