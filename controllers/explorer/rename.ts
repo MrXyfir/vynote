25d445b5f321c5f98400f4fceb5ba03e036bdb10
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-02-21 11:41:32 -0800

    Moved data arguments into a single object. Decreased object name length. Cleaned up SQL statement

--- controllers/explorer/rename.ts
+++ controllers/explorer/rename.ts
@@ -1,22 +1,28 @@
 ﻿import db = require("../../lib/db");
 
-export = (socket: SocketIO.Socket, objType: number, id: number, name: string, fn: Function) => {
+interface IData {
+    objType: number, id: number, name: string
+}
+
+export = (socket: SocketIO.Socket, data: IData, fn: Function) => {
 
     // Validate name
-    if (!name.match(/^[\w\d-.,#$%&()]{1,100}$/)) {
+    if (!data.name.match(/^[\w\d-.,#$%&()]{1,50}$/)) {
         fn(true, "Invalid name characters or length");
     }
     // Contributor document
-    else if (objType == 3) {
+    else if (data.objType == 3) {
         fn(true, "Only the file creator can rename this file");
     }
     // Document or folder
     else {
         // Update folders|documents where folder_id|doc_id
-        let sql: string = "UPDATE " + (objType == 1 ? "folders" : "documents") +
-            " SET name = ? WHERE " + (objType == 1 ? "folder_id" : "doc_id") + " = ? AND user_id = ?";
+        let sql: string = `
+            UPDATE ${data.objType == 1 ? "folders" : "documents"} 
+            SET name = ? WHERE ${data.objType == 1 ? "folder_id" : "doc_id"} = ? AND user_id = ?
+        `;
 
-        db(cn => cn.query(sql, [name, id, socket.session.uid], (err, result) => {
+        db(cn => cn.query(sql, [data.name, data.id, socket.session.uid], (err, result) => {
             cn.release();
 
             fn(!!err || !result.affectedRows);
