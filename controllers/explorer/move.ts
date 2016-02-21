727183fde2577527b9956342f4bc3e529e8b305b
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-02-21 14:44:05 -0800

    Moved data into data object

--- controllers/explorer/move.ts
+++ controllers/explorer/move.ts
@@ -1,40 +1,41 @@
 ﻿import db = require("../../lib/db");
 
-export = (socket: SocketIO.Socket, objType: number, id: number, to: number, fn: Function) => {
+interface IData {
+    objType: number, id: number, to: number
+}
+
+export = (socket: SocketIO.Socket, data: IData, fn: Function) => {
+
+    if (data.objType != 1 && data.to == 0) {
+        fn(true, "Cannot move document to root folder");
+        return;
+    }
 
     db(cn => {
         let sql: string = "";
 
         // Ensure that user owns folder they want to move object to
         sql = "SELECT * FROM folders WHERE user_id = ? AND folder_id = ?";
-        cn.query(sql, [socket.session.uid, to], (err, rows) => {
-            if ((err || !rows.length) && to != 0) {
+        cn.query(sql, [socket.session.uid, data.to], (err, rows) => {
+            if ((err || !rows.length) && data.to != 0) {
                 cn.release();
                 fn(true, "Folder does not exist in your account");
             }
             else {
                 // Folder
-                if (objType == 1)
+                if (data.objType == 1)
                     sql = "UPDATE folders SET parent_id = ? WHERE folder_id = ? AND user_id = ?";
                 // Document
-                else if (objType == 2)
+                else if (data.objType == 2)
                     sql = "UPDATE documents SET folder_id = ? WHERE doc_id = ? AND user_id = ?";
                 // Contributor Document
-                else if (objType == 3)
+                else if (data.objType == 3)
                     sql = "UPDATE document_contributors SET folder_id = ? WHERE doc_id = ? AND user_id = ?";
 
-                cn.query(sql, [to, id, socket.session.uid], (err, result) => {
+                cn.query(sql, [data.to, data.id, socket.session.uid], (err, result) => {
                     cn.release();
 
-                    if (!!err || !result.affectedRows) {
-                        if (objType != 1 && to == 0)
-                            fn(true, "Cannot move document to root folder");
-                        else
-                            fn(true);
-                    }
-                    else {
-                        fn(false);
-                    }
+                    fn(!!err || !result.affectedRows);
                 });
             }
         });
