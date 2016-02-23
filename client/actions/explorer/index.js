d66279b2c4c928d83dcae02db4eac028d172bad0
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-02-22 17:11:26 -0800

    Add createFolder, createDocument, deleteObject actions

--- client/actions/explorer/index.js
+++ client/actions/explorer/index.js
@@ -1,14 +1,16 @@
-import { LOAD_FILESYSTEM, UPDATE_SCOPE } from "../../constants/action-types/explorer/";
+import {
+	LOAD_FILESYSTEM, UPDATE_SCOPE, DELETE_OBJECT, CREATE_FOLDER, CREATE_DOCUMENT
+} from "../../constants/action-types/explorer/";
 
 export function createFolder(data) {
 	return {
-		
+		type: CREATE_FOLDER, data
 	};
 }
 
 export function createDocument(data) {
 	return {
-		
+		type: CREATE_DOCUMENT, data
 	};
 }
 
@@ -21,5 +23,11 @@ export function loadFileSystem(explorer) {
 export function navigateToFolder(scope) {
 	return {
 		type: UPDATE_SCOPE, scope
+	};
+}
+
+export function deleteObject(objType, id) {
+	return {
+		type: DELETE_OBJECT, objType, id
 	};
 }
