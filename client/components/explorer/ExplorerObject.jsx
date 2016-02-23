961a83144d021fb9cd3bbf7a7dd5f49047ad95f0
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-02-23 10:52:31 -0800

    Added ExplorerObject for handling each file/folder in Explorer

---
+++ client/components/explorer/ExplorerObject.jsx
@@ -0,0 +1,145 @@
+import { Component } from "react";
+
+// Action creators
+import {
+	triggerRenameObject, triggerMoveObject
+} from "../../actions/explorer/user-input";
+import { error } from "../../actions/notification";
+import { navigateToFolder, deleteObject } from "../../actions/explorer/";
+import { loadDocument } from "../../actions/document/";
+
+// Constants
+import colors from "../../constants/colors";
+
+// Modules
+import buildNoteObject from "../../../lib/notes-convert/to-object";
+
+export default class ExplorerObject extends Component {
+
+	constructor(props) {
+		super(props);
+		
+		this.state = { showControls: false };
+	}
+	
+	onMouseOver() {
+		this.setState({ showControls: true });
+	}
+	
+	onMouseOut() {
+		this.setState({ showControls: false });
+	}
+
+	onRename() {
+		triggerRenameObject(
+			(this.props.isDoc ? 2 : 1),
+			this.props.data[this.props.isDoc ? "doc_id" : "folder_id"]
+		);
+	}
+	
+	onMove() {
+		triggerMoveObject(
+			(this.props.isDoc ? 2 : 1),
+			this.props.data[this.props.isDoc ? "doc_id" : "folder_id"]
+		);
+	}
+	
+	onDelete() {
+		let type = this.props.isDoc ? 2 : 1;
+		let id = this.props.data[type == 2 ? "doc_id" : "folder_id"]
+		
+		this.props.emit("delete object", type, id, (err, msg) => {
+			if (err) {
+				this.dispatch(error(msg));
+			}
+			else {
+				this.dispatch(deleteObject(type, id));
+			}
+		});
+	}
+	
+	onOpen() {
+		if (this.props.isDoc) {
+			// If document is encrypted, Document container will handle content loading
+			// All we need to set is the type and id
+			if (this.props.data.encrypted) {
+				// Document container will require encryption key when
+				// encrypted == true && encrypt == ""
+				this.dispatch(loadDocument(
+					Object.assign({}, this.props.data, { encrypt: "" }
+				);
+				return;
+			}
+		
+			// Note document
+			if (this.props.data.doc_type == 1) {
+				this.props.emit("get note elements", this.props.data.doc_id, "", (err, res) => {
+					if (err) {
+						this.dispatch(error("Could not load note"));
+					}
+					else {
+						this.dispatch(loadDocument(
+							Object.assign({}, this.props.data, {
+								content: buildNoteObject(res)
+							}
+						));
+					}
+				});
+			}
+			// Other document
+			else {
+				this.props.emit("get document content", this.props.data.doc_id, "", (err, res) => {
+					if (err) {
+						this.dispatch(error("Could not load document"));
+					}
+					else {
+						this.dispatch(loadDocument(
+							Object.assign({}, this.props.data, { content: res })
+						);
+					}
+				});
+			}
+		}
+		else {
+			this.dispatch(navigateToFolder(this.props.data.folder_id));
+		}
+	}
+
+	render() {
+		let icon = "";
+		if (this.props.isDoc) {
+			switch (this.props.data.doc_type) {
+				case 1: icon = "nested-note"; break;
+				case 2: icon = "doc-text"; break;
+				case 3: icon = "file-code";
+			}
+		}
+		else {
+			icon = "folder";
+		}
+	
+		return (
+			<div 
+				className={"explorer-object-" + (this.props.isDoc ? "document" : "folder")}
+				onMouseOver={this.onMouseOver} onMouseOut={this.onMouseOut} 
+			>
+				<span className={"icon-" + icon} style={{color: colors[this.props.data.color]}} />
+				<span className="object-name" onClick={this.props.onOpen}>{this.props.data.name}</span>
+				{
+					this.state.showControls
+					? (
+						<div className="object-controls">
+							<span className="icon-edit" onClick={this.onRename} />
+							<span className="icon-move" onClick={this.onMove} />
+							<span className="icon-trash" onClick={this.onDelete} />
+						</div>
+					)
+					: (
+						<span className="object-controls-hidden" />
+					)
+				}
+			</div>
+		);
+	}
+
+}
--- client/containers/Explorer.jsx
+++ client/containers/Explorer.jsx
@@ -1,9 +1,9 @@
 import { Component } from "react";
 
 import DirectoryAddress from "../components/explorer/DirectoryAddress";
+import ExplorerObject from "../components/explorer/ExplorerObject";
 import ControlBar from "../components/explorer/ControlBar";
 import UserInput from "../components/explorer/UserInput";
-import Object from "../components/explorer/Object";
 
 export default class Explorer extends Component {
 	
@@ -16,26 +16,20 @@ export default class Explorer extends Component {
 					emit={this.props.emit} 
 					dispatch={this.props.dispatch}
 				/>
-				<div className="explorer-objects">
-					<div className="explorer-objects-folders">{
-						Object.keys(this.props.folders).map(id => {
-							return (<Object 
-								data={this.props.folders[id]}
+				<div className="explorer-objects">{
+					this.props.data.children[this.props.data.scope].map(child => {
+						let isDoc = (child.type == 2);
+						
+						return (
+							<ExplorerObject
+								data={this.props.data[isDoc ? "documents" : "folders"][child.id]} 
 								emit={this.props.emit} 
+								isDoc={isDoc} 
 								dispatch={this.props.dispatch}
-							/>);
-						})
-					}</div>
-					<div className="explorer-objects-documents">{
-						Object.keys(this.props.documents).map(id => {
-							return (<Object 
-								data={this.props.documents[id]}
-								emit={this.props.emit} 
-								dispatch={this.props.dispatch}
-							/>);
-						})
-					}</div>
-				</div>
+							/>
+						);
+					})
+				}</div>
 				<UserInput 
 					data={this.props.data} 
 					emit={this.props.emit} 
