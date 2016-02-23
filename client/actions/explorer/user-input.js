a5fbef40138e148e90d63d3f786299c07bd784a2
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-02-23 10:44:46 -0800

    Add trigger[Rename|Remove]Object actions

--- client/actions/explorer/user-input.js
+++ client/actions/explorer/user-input.js
@@ -1,5 +1,5 @@
 import { 
-	CREATE_FOLDER, CREATE_DOCUMENT, CLOSE
+	CREATE_FOLDER, CREATE_DOCUMENT, CLOSE, RENAME_OBJECT, MOVE_OBJECT
 } from "../../constants/action-types/explorer/user-input";
 
 export function triggerCreateFolder() {
@@ -18,4 +18,16 @@ export function closeUserInput() {
 	return {
 		type: CLOSE
 	};
+}
+
+export function triggerRenameObject(objType, objId) {
+	return {
+		type: RENAME_OBJECT, objType, objId
+	}
+}
+
+export function triggerMoveObject(objType, objId) {
+	return {
+		type: MOVE_OBJECT, objType, objId
+	}
 }
