009c43331ad0594e8dea67df2ff61a17267b17be
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-02-25 14:12:18 -0800

    Document version control actions

---
+++ client/actions/modal/document-controls/versions.js
@@ -0,0 +1,21 @@
+import {
+	LOAD_VERSIONS, DELETE_VERSION, CREATE_VERSION
+} from "../../../constants/action-types/modal/document-controls/versions";
+
+export function loadVersions(versions) {
+	return {
+		type: LOAD_VERSIONS, versions
+	};
+}
+
+export function deleteVersion(name) {
+	return {
+		type: DELETE_VERSION, name
+	};
+}
+
+export function createVersion(name) {
+	return {
+		type: CREATE_VERSION, name
+	};
+}
