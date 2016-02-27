5ad49ba9432e74b7363be567538cdc64b13abcfc
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-02-26 17:19:48 -0800

    Added Export Document component

---
+++ client/components/modal/document-controls/Export.jsx
@@ -0,0 +1,51 @@
+import { Component } from "react";
+
+// Action creators
+import { error, success } from "../../../actions/notification";
+
+// Modules
+import objectToText from "../../../../lib/notes-convert/to-text";
+
+export default class Export extends Component {
+
+	constructor(props) {
+		super(props);
+	}
+	
+	onExport() {
+		if (Date.now() > this.props.data.user.subscription) {
+			this.props.dispatch(error("Free members cannot export documents"));
+			return;
+		}
+	
+		let file = "", content = "";
+		
+		if (this.props.data.document.doc_type == 1)
+			content = objectToText(this.props.data.document);
+		else
+			content = this.props.data.document.content;
+	
+		file = this.props.data.document.name + ".txt";
+	
+		let dl = document.createElement('a');
+		dl.setAttribute('href', 'data:text/plain;charset=utf-8,' + encodeURIComponent(content));
+		dl.setAttribute('download', file);
+		dl.style.display = 'none';
+		
+		document.body.appendChild(dl);
+		dl.click();
+		document.body.removeChild(dl);
+		
+		this.props.dispatch(success(`Exported document '${this.props.data.document.name}'`));
+	}
+	
+	render() {
+		return (
+			<div className="document-export">
+				<p>Convert and download a local version of this document.</p>
+				<button onClick={this.onExport}>Export Document</button>
+			</div>
+		);
+	}
+
+}
