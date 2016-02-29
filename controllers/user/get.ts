512cbf8b12b0c3d87ce269665880be8a59f1cacc
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-02-29 10:00:20 -0800

    Added is-user-logged-in check and pass result to client

--- controllers/user/get.ts
+++ controllers/user/get.ts
@@ -2,22 +2,30 @@
 
 export = (socket: SocketIO.Socket, fn: Function) => {
 
+    if (!socket.session.uid) {
+        fn(false);
+        return;
+    }
+
     db(cn => {
         let sql: string = "SELECT email, config, subscription FROM users WHERE user_id = ?";
         cn.query(sql, [socket.session.uid], (err, rows) => {
+            if (err || !rows.length) {
+                fn(false);
+            }
             // Grab user's shortcuts if user has subscription
-            if (socket.session.subscription > Date.now()) {
+            else if (socket.session.subscription > Date.now()) {
                 sql = "SELECT name, content FROM shortcuts WHERE user_id = ?";
                 cn.query(sql, [socket.session.uid], (err, shortcuts) => {
                     cn.release();
 
                     rows[0].shortcuts = shortcuts;
-                    fn(rows[0]);
+                    fn(true, rows[0]);
                 });
             }
             else {
                 cn.release();
-                fn(rows[0]);
+                fn(true, rows[0]);
             }
         });
     });
