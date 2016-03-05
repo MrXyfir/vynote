822b9653ab38d61690c758605f78356e1614bae2
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-03-05 10:31:52 -0800

    SET_THEME no longer sets theme as syntax

--- client/reducers/document/code.js
+++ client/reducers/document/code.js
@@ -3,9 +3,11 @@ import { SET_SYNTAX, SET_THEME } from "../../constants/action-types/documents/co
 export default function (state, action) {
 	switch (action.type) {
 		case SET_SYNTAX:
-			return Object.assign({}, state, { syntax: action.syntax });
+            return Object.assign({}, state, { syntax: action.syntax });
+            
 		case SET_THEME:
-			return Object.assign({}, state, { syntax: action.theme });
+            return Object.assign({}, state, { theme: action.theme });
+            
 		default:
 			return state;
 	}
