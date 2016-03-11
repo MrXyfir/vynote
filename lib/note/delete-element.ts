a5fd7091d11afb52025564d0b6429e768c219046
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-03-11 13:43:41 -0800

    Add module for deleting an element and its children from note object

---
+++ lib/note/delete-element.ts
@@ -0,0 +1,34 @@
+﻿interface IData { id: string }
+
+export = (note: any, data: IData) => {
+
+    let parent: string = note.notes[data.id].parent;
+
+    // Remove data.id from children of parent
+    note.notes[parent].children = note.notes[parent].children.filter(child => {
+        return child != data.id;
+    });
+
+    let children: string[] = [];
+
+    // Populate children[] with ids of all children elements
+    const getChildren = (id: string): string[] => {
+        let c: string[] = [];
+
+        note.notes[id].children.forEach(child => {
+            c.concat(getChildren(child));
+        });
+
+        return c;
+    };
+    children = getChildren(data.id);
+
+    // Delete child elements in children[]
+    children.forEach(child => {
+        delete note.notes[child];
+    });
+
+    // Delete data.id
+    delete note.notes[data.id];
+
+}
