e7bf86268cf6b0219136c96ac60996fa94e5462c
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-03-11 13:27:22 -0800

    Creates a new element with id/content under parent in note object

---
+++ lib/note/create-element.ts
@@ -0,0 +1,14 @@
+﻿interface IData {
+    id: string, content: string, parent: string
+}
+
+export = (note: any, data: IData) => {
+
+    note.notes[data.id] = {
+        content: data.content, parent: data.parent, flags: [],
+        showChildren: false, children: []
+    };
+
+    note.notes[data.parent].children.push(data.id);
+
+}
