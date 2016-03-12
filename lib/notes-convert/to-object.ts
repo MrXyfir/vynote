0c473a6376b8cbcc9ca52fcff79625938135af52
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-03-11 19:19:26 -0800

    Rewrite to build object from text

--- lib/notes-convert/to-object.ts
+++ lib/notes-convert/to-object.ts
@@ -1,32 +1,64 @@
-﻿interface INoteElement {
-    note_id: number, parent_id: number, content: string
+﻿import randString = require("../rand-string");
+
+function countLevels(text: string): number {
+    let count: number = 0;
+    let index: number = 0;
+
+    while (true) {
+        if (text.charAt(index++) === "\t")
+            count++;
+        else
+            return count;
+    }
 }
 
-// Convert note elements to an object that can be easily rendered
-// in multiple forms (text, JSX, etc)
-export = (elements: INoteElement[]) => {
-    let note = {
-        ref: {
-            // id: [child: number, child2: number, ...]
-        },
-        notes: {
-            // id: { parent: number, content: string }
+export = (lines: string[]) => {
+
+    let notes = {
+        home: {
+            content: "HOME", children: []
         }
     };
 
-    elements.forEach(element => {
-        // Add note's parent and content to note.notes object by ID
-        note.notes[element.note_id] = {
-            parent: element.parent_id, content: element.content
+    let parents: string[] = ["home"];
+    let parent: string = "home";
+    let level: number = 0;
+    let id: string = "";
+    
+    lines.forEach((line: string, i: number) => {
+
+        // Generate unique element id
+        while (true) {
+            id = randString(10);
+            if (notes[id] === undefined)
+                break;
+        }
+
+        // Determine line's level based on tabs in string
+        // +1 due to zero-index and 0 being the default "home" parent level
+        level = countLevels(line) + 1;
+
+        // Add current note's id to parents[level]
+        parents[level] = id;
+
+        // Delete all parents in a level deeper than the current level
+        if (parents.length > level)
+            parents.splice(level + 1, parents.length - level);
+
+        // The note's parent_id is equal to parents[currentLevel - 1]
+        parent = parents[level - 1];
+
+        // Create element in notes object
+        notes[id] = {
+            content: line.substr(level + 1), parent, flags: [], children: []
         };
 
-        // Add note as child for element.parent_id in ref
-        if (note.ref[element.parent_id] === undefined)
-            note.ref[element.parent_id] = [element.note_id];
-        else
-            note.ref[element.parent_id].push(element.note_id);
+        // Add id to parent
+        notes[parent].children.push(id);
+        
+        lines[i] = "";
     });
-    elements = null;
 
-    return note;
-}
+    return notes;
+
+};
