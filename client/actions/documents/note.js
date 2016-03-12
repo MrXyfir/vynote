e43cebbe869ddc525a53ed88feebf03f9e2fc3c8
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-03-12 09:14:03 -0800

    Add action/reducer for initializing the state.document.render object

---
+++ client/actions/documents/note.js
@@ -0,0 +1,9 @@
+import {
+    INITIALIZE_RENDER
+} from "../../constants/action-types/documents/note";
+
+export function initializeRenderObject() {
+    return {
+        type: INITIALIZE_RENDER
+    };
+}
--- client/reducers/document/note.js
+++ client/reducers/document/note.js
@@ -1,3 +1,23 @@
+import {
+    INITIALIZE_RENDER
+} from "../../constants/action-types/documents/note";
+
 export default function (state, action) {
-    return state;
+    switch (action.type) {
+        case INITIALIZE_RENDER:
+            return Object.assign({}, state, {
+                render: {
+                    scope: "home",
+                    scopeParents: [],
+                    filter: {
+                        flags: [], search: ""
+                    },
+                    showChildren: [],
+                    editing: "", hovering: "", controls: ""
+                }
+            });
+        
+        default:
+            return state;
+    }
 }
