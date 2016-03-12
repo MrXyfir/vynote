07e41c5e9bc0f903607fe008ac1ea0ce569c77ee
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-03-11 19:32:43 -0800

    Update to work with new note object/storage system

--- controllers/notes/import.ts
+++ controllers/notes/import.ts
@@ -1,4 +1,4 @@
-﻿import toSql = require("../../lib/notes-convert/to-sql");
+﻿import toObject = require("../../lib/notes-convert/to-object");
 import db = require("../../lib/db");
 
 export = (socket: SocketIO.Socket, note: number, content: string[], fn: Function) => {
@@ -33,19 +33,26 @@ export = (socket: SocketIO.Socket, note: number, content: string[], fn: Function
             fn(true, "You do not have access to this document");
         }
         else {
-            // Delete all note elements for document
-            sql = "DELETE FROM note_elements WHERE doc_id = ?"
-            cn.query(sql, [note], (err, result) => {
-                // Convert content to SQL 'INSERT INTO' statement
-                let insert: string = toSql(note, content);
-                content = null;
+            let insert: string = JSON.stringify(toObject(content));
+            content = null;
 
-                // Insert new rows
-                cn.query(sql, (err, result) => {
+            sql = "UPDATE document_content SET content = ? WHERE doc_id = ?";
+            cn.query(sql, [insert, note], (err, result) => {
+                if (err || !result.affectedRows) {
                     cn.release();
+                    fn(true);
+                }
+                else {
+                    sql = "DELETE FROM document_changes WHERE doc_id = ?";
+                    cn.query(sql, [note], (err, result) => {
+                        cn.release();
+                        fn(false);
 
-                    fn(!!err || !result.affectedRows);
-                });
+                        socket.broadcast.to('' + note).emit("note change", {
+                            action: "OVERWRITE", content: insert
+                        });
+                    });
+                }
             });
         }
     }));
