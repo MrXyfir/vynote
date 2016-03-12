8268d564bf9a311ad68f0be663ff396625da0e50
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-03-11 18:44:08 -0800

    Remove showChildren property

--- lib/note/create-element.ts
+++ lib/note/create-element.ts
@@ -5,8 +5,8 @@
 export = (notes: any, data: IData) => {
 
     notes[data.id] = {
-        content: data.content, parent: data.parent, flags: [],
-        showChildren: false, children: []
+        content: data.content, parent: data.parent,
+        flags: [], children: []
     };
 
     notes[data.parent].children.push(data.id);
