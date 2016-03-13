d3bfded444cda85f9d5b01ea5c06d09dbd6a8efd
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-03-12 16:48:16 -0800

    Now initial state.document.render.filter.flags is all flags other than "Hidden"

--- client/reducers/document/note.js
+++ client/reducers/document/note.js
@@ -1,9 +1,13 @@
+// Action types
 import {
     INITIALIZE_RENDER, CHANGE_SCOPE
 } from "../../constants/action-types/documents/note";
 
 // Modules
 import scopeParents from "../../lib/note/scopeParents";
+
+// Other constants
+import flags from "../../constants/flags";
 
 export default function (state, action) {
     switch (action.type) {
@@ -13,7 +17,8 @@ export default function (state, action) {
                     scope: "home",
                     scopeParents: [],
                     filter: {
-                        flags: [], search: ""
+                        flags: flags.slice(1),
+                        search: ""
                     },
                     showChildren: [],
                     editing: "", hovering: "", controls: ""
