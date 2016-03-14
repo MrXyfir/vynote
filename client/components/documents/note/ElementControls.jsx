84e19a7f66ba80a100c2bd3d67552c26bf0976bd
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-03-14 14:51:24 -0700

    Add code for note element controls

--- client/components/documents/note/ElementControls.jsx
+++ client/components/documents/note/ElementControls.jsx
@@ -1,63 +1,154 @@
 import React from "react";
 
+// Action creators
+import {
+    toggleShowChildren, navigateToElement, showElementControls,
+    setElementFlags, addElement, deleteElement, editElement
+} from "../../../actions/documents/note";
+import { error } from "../../../actions/notification";
+
+// Constants
+import flags from "../../../constants/flags";
+
+// Modules
+import generateID from "../../../lib/note/generate-id";
+
 export default class ElementControls extends React.Component {
     
     constructor(props) {
         super(props);
         
-        this.onShowExtendedControls = this.onShowExtendedControls.bind(this);
         this.onToggleShowChildren = this.onToggleShowChildren.bind(this);
         this.onScopeToElement = this.onScopeToElement.bind(this);
+        this.onMouseOver = this.onMouseOver.bind(this);
+        this.onMouseOut = this.onMouseOut.bind(this);
+        this.onAddChild = this.onAddChild.bind(this);
+        this.onDelete = this.onDelete.bind(this);
     }
     
-    onShowExtendedControls() {
+    onMouseOver() {
+        this.timeout = setTimeout(() => {
+            this.props.dispatch(showElementControls(this.props.id));
+        }, 500);
+    }
+    
+    onMouseOut() {
+        clearTimeout(this.timeout);
         
+        // Check if element's controls were open
+        if (this.props.data.render.controls == this.props.id) {
+            this.props.dispatch(showElementControls(""));
+            
+            let changed = false;
+            
+            // Check if element's flags have been changed
+            flags.forEach((flag, i) => {
+                let oldF = this.props.data.content[this.props.id].flags.indexOf(i) > -1;
+                let newF = this.refs[`flag-${i}`].checked;
+                
+                if (oldF !== newF) changed = true;
+            });
+            
+            // Update element's flags
+            if (changed) {
+                let data = {
+                    action: "SET_FLAGS", doc: this.props.data.document.doc_id,
+                    id: this.props.id, content: flags.map((flag, i) => {
+                        return this.refs[`flag-${i}`].checked;
+                    }).filter(val => { return val; })
+                };
+                
+                this.props.emit("change note element", data, (err, res) => {
+                    if (err) {
+                        this.props.dispatch(error(res));
+                    }
+                    else {
+                        this.props.dispatch(setElementFlags(
+                            this.props.id, data.flags
+                        ));
+                    }
+                });
+            }
+        }
     }
     
     onToggleShowChildren() {
-        
+        this.props.dispatch(toggleShowChildren(this.props.id));
     }
     
     onScopeToElement() {
+        this.props.dispatch(navigateToElement(this.props.id));
+    }
+    
+    onAddChild() {
+        let id = generateID(this.props.data.content);
         
+        // Create child element
+        this.props.dispatch(addElement(this.props.id, id));
+        
+        // Show current element's children
+        if (this.props.data.render.showChildren.indexOf(this.props.id) == -1) {
+            this.props.dispatch(toggleShowChildren(this.props.id));
+        }
+        
+        // Set child element editing
+        this.props.dispatch(editElement(id));
+    }
+    
+    onDelete() {
+        this.props.dispatch(deleteElement(this.props.id));
     }
     
     render() {
         return (
-            <div className="note-controls">
-            { // Output 'Show Children' toggle if element is being hovered
-                this.props.id == this.props.data.render.hovering
-                ? (
-                    this.props.data.render.showChildren.indexOf(this.props.id) > -1
-                    ? (<span onClick={this.onToggleShowChildren}>-</span>)
-                    : (<span onClick={this.onToggleShowChildren}>+</span>)
-                )
-                : (
-                    <span className="hidden" />
-                )
-            }
-            <span 
-                onClick={this.onScopeToElement}
-                className="icon-circle" 
-                onContextMenu={this.onShowExtendedControls} 
-            />
-            
-            { // Optionally render extended controls
-                this.props.id == this.props.data.render.controls
-                ? (
-                    <div className="controls-extended">
-                        Delete
-                        Flags
-                        Add Child
-                        Duplicate
-                        Export
-                    </div>
-                )
-                : (
-                    <span className="hidden" />
-                )
-            }
-        </div>
+            <div className="note-controls" onMouseOut={this.onMouseOut}>
+                { // Output 'Show Children' toggle if element is being hovered
+                    this.props.id == this.props.data.render.hovering
+                    ? (
+                        this.props.data.render.showChildren.indexOf(this.props.id) > -1
+                        ? (<span onClick={this.onToggleShowChildren} title="Hide Children">-</span>)
+                        : (<span onClick={this.onToggleShowChildren} title="Show Children">+</span>)
+                    )
+                    : (
+                        <span className="hidden" />
+                    )
+                }
+                <span
+                    title="Scope to Element"  
+                    onClick={this.onScopeToElement}
+                    className="icon-circle" 
+                    onMouseOver={this.onMouseOver} 
+                />
+                
+                { // Optionally render extended controls
+                    this.props.id == this.props.data.render.controls
+                    ? (
+                        <div className="controls-extended">
+                            <span onClick={this.onDelete}>Delete</span>
+                            <span onClick={this.onAddChild}>Add Child</span>
+                            <div className="flags">{
+                                flags.map((flag, i) => {
+                                    return (
+                                        <div className="flag">
+                                            <input 
+                                                ref={`flag-${i}`} 
+                                                type="checkbox" 
+                                                defaultChecked={
+                                                    this.props.data.content[this.props.id].flags.indexOf(i) > -1
+                                                } 
+                                            />
+                                            <span>{flag}</span>
+                                        </div>
+                                    );
+                                })
+                            }</div>
+                        </div>
+                    )
+                    : (
+                        <span className="hidden" />
+                    )
+                }
+            </div>
         );
     }
     
