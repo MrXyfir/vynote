cca23865413779c7a802a309f28f836281ab18bd
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-03-15 19:58:10 -0700

    Extended controls now open onContextMenu and close when close button is clicked. onMouseOver/Out is now used to set hoverElement()

--- client/components/documents/note/ElementControls.jsx
+++ client/components/documents/note/ElementControls.jsx
@@ -3,7 +3,8 @@ import React from "react";
 // Action creators
 import {
     toggleShowChildren, navigateToElement, showElementControls,
-    setElementFlags, addElement, deleteElement, editElement
+    setElementFlags, addElement, deleteElement, editElement,
+    hoverElement
 } from "../../../actions/documents/note";
 import { error } from "../../../actions/notification";
 
@@ -18,6 +19,8 @@ export default class ElementControls extends React.Component {
     constructor(props) {
         super(props);
         
+        this.onCloseExtendedControls = this.onCloseExtendedControls.bind(this);
+        this.onShowExtendedControls = this.onShowExtendedControls.bind(this);
         this.onToggleShowChildren = this.onToggleShowChildren.bind(this);
         this.onScopeToElement = this.onScopeToElement.bind(this);
         this.onMouseOver = this.onMouseOver.bind(this);
@@ -27,48 +30,53 @@ export default class ElementControls extends React.Component {
     }
     
     onMouseOver() {
-        this.timeout = setTimeout(() => {
-            this.props.dispatch(showElementControls(this.props.id));
-        }, 500);
+        this.props.dispatch(hoverElement(this.props.id));
     }
     
     onMouseOut() {
-        clearTimeout(this.timeout);
+        this.props.dispatch(hoverElement(""));
+    }
+    
+    onShowExtendedControls(e) {
+        e.preventDefault();
+        this.props.dispatch(showElementControls(this.props.id));
+    }
+    
+    onCloseExtendedControls() {
+        let changed = false;
         
-        // Check if element's controls were open
-        if (this.props.data.render.controls == this.props.id) {
-            this.props.dispatch(showElementControls(""));
+        // Check if element's flags have been changed
+        flags.forEach((flag, i) => {
+            let oldF = this.props.data.content[this.props.id].flags.indexOf(i) > -1;
+            let newF = this.refs[`flag-${i}`].checked;
             
-            let changed = false;
+            if (oldF !== newF) changed = true;
+        });
+        
+        // Update element's flags
+        if (changed && !this.props.data.content[this.props.id].create) {
+            let data = {
+                action: "SET_FLAGS", doc: this.props.data.document.doc_id,
+                id: this.props.id, content: flags.map((flag, i) => {
+                    return this.refs[`flag-${i}`].checked;
+                }).filter(val => { return val; })
+            };
             
-            // Check if element's flags have been changed
-            flags.forEach((flag, i) => {
-                let oldF = this.props.data.content[this.props.id].flags.indexOf(i) > -1;
-                let newF = this.refs[`flag-${i}`].checked;
+            this.props.emit("change note element", data, (err, res) => {
+                if (err) {
+                    this.props.dispatch(error(res));
+                }
+                else {
+                    this.props.dispatch(setElementFlags(
+                        this.props.id, data.flags
+                    ));
+                }
                 
-                if (oldF !== newF) changed = true;
+                this.props.dispatch(showElementControls(""));
             });
-            
-            // Update element's flags
-            if (changed && !this.props.data.content[this.props.id].create) {
-                let data = {
-                    action: "SET_FLAGS", doc: this.props.data.document.doc_id,
-                    id: this.props.id, content: flags.map((flag, i) => {
-                        return this.refs[`flag-${i}`].checked;
-                    }).filter(val => { return val; })
-                };
-                
-                this.props.emit("change note element", data, (err, res) => {
-                    if (err) {
-                        this.props.dispatch(error(res));
-                    }
-                    else {
-                        this.props.dispatch(setElementFlags(
-                            this.props.id, data.flags
-                        ));
-                    }
-                });
-            }
+        }
+        else {
+            this.props.dispatch(showElementControls(""));
         }
     }
     
@@ -125,7 +133,11 @@ export default class ElementControls extends React.Component {
     
     render() {
         return (
-            <div className="note-controls" onMouseOut={this.onMouseOut}>
+            <div 
+                className="note-controls" 
+                onMouseOver={this.onMouseOver}
+                onMouseOut={this.onMouseOut} 
+            >
                 { // Output 'Show Children' toggle if element is being hovered
                     this.props.id == this.props.data.render.hovering
                     ? (
@@ -140,14 +152,22 @@ export default class ElementControls extends React.Component {
                 <span
                     title="Scope to Element"  
                     onClick={this.onScopeToElement}
-                    className="icon-circle" 
-                    onMouseOver={this.onMouseOver} 
+                    className={
+                        (this.props.id == this.props.data.render.controls)
+                        ? "icon-circle highlight" : "icon-circle-outline"
+                    } 
+                    onContextMenu={this.onShowExtendedControls} 
                 />
                 
                 { // Optionally render extended controls
                     this.props.id == this.props.data.render.controls
                     ? (
                         <div className="controls-extended">
+                            <span 
+                                title="Close Controls"
+                                onClick={this.onCloseExtendedControls} 
+                                className="icon-close" 
+                            />
                             <span onClick={this.onDelete}>Delete</span>
                             <span onClick={this.onAddChild}>Add Child</span>
                             <div className="flags">{
