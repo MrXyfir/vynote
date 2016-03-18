dfaa657b73d2c33b79bc6258f07bcd5900c75713
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-03-18 12:21:02 -0700

    Add 'toggle show upgrade form' action/reducer

---
+++ client/actions/modal/user/account.js
@@ -0,0 +1,9 @@
+import {
+    TOGGLE_SHOW_UPGRADE_FORM
+} from "../../../constants/action-types/modal/user/account";
+
+export function toggleShowUpgradeForm() {
+    return {
+        type: TOGGLE_SHOW_UPGRADE_FORM
+    };
+}
---
+++ client/reducers/modal/user/account.js
@@ -0,0 +1,13 @@
+import {
+    TOGGLE_SHOW_UPGRADE_FORM
+} from "../../../constants/action-types/modal/user/account";
+
+export default function (state, action) {
+    switch (action.type) {
+        case TOGGLE_SHOW_UPGRADE_FORM:
+            return Object.assign({}, state, { showUpgradeForm: !state.showUpgradeForm });
+        
+        default:
+            return state;
+    }
+}
