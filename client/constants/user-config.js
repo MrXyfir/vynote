91153dbf9603165f1a49cef0d89db8b69138462c
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-03-19 10:06:11 -0700

    Add default state.user.config object

---
+++ client/constants/user-config.js
@@ -0,0 +1,9 @@
+export default {
+    defaultExplorerObjectColor: 7,
+    defaultDocumentType: "0",
+    defaultCodeSyntax: 70,
+    defaultEditorTheme: 6,
+    defaultPageView: "preview",
+    editorFontSize: 1,
+    darkTheme: false
+};
