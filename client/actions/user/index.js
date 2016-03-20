7d01fc4e8754a568bc72281ceb29b2378798f04b
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-03-19 17:55:43 -0700

    Add "set config", "create shortcut", "delete shortcut" actions/reducers

--- client/actions/user/index.js
+++ client/actions/user/index.js
@@ -1,7 +1,27 @@
-import { SET_SUBSCRIPTION } from "../../constants/action-types/user/";
+import {
+    SET_SUBSCRIPTION, SET_CONFIG, CREATE_SHORTCUT, DELETE_SHORTCUT
+} from "../../constants/action-types/user/";
 
 export function setSubscription(time) {
     return {
         type: SET_SUBSCRIPTION, time
+    };
+}
+
+export function setConfig(config) {
+    return {
+        type: SET_CONFIG, config
+    };
+}
+
+export function createShortcut(name, message) {
+    return {
+        type: CREATE_SHORTCUT, name, message
+    };
+}
+
+export function deleteShortcut(name) {
+    return {
+        type: DELETE_SHORTCUT, name
     };
 }
--- client/reducers/user/index.js
+++ client/reducers/user/index.js
@@ -1,13 +1,30 @@
 // Action types
 import {
-    SET_SUBSCRIPTION
+    SET_SUBSCRIPTION, SET_CONFIG, CREATE_SHORTCUT, DELETE_SHORTCUT
 } from "../../constants/action-types/user/";
 
 export default function (state, action) {
 
     switch (action.type) {
         case SET_SUBSCRIPTION:
-            return Object.assign({}, state, { subscription: action.time});
+            return Object.assign({}, state, { subscription: action.time });
+            
+        case SET_CONFIG:
+            return Object.assign({}, state, { config: action.config });
+            
+        case CREATE_SHORTCUT:
+            return Object.assign({}, state, {
+                shortcuts: Object.assign({}, state.shortcuts, {
+                    [action.name]: action.message
+                })
+            });
+            
+        case DELETE_SHORTCUT:
+            return (() => {
+                let temp = Object.assign({}, state);
+                delete temp.shortcuts[action.name];
+                return temp;
+            }).call();
         
         default:
             return state;
