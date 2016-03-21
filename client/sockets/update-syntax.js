e1900a0e9e36b3ef16ea9dc001ea024cd67b574c
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-03-21 15:47:13 -0700

    Add controller for updating document syntax

--- client/sockets/update-syntax.js
+++ client/sockets/update-syntax.js
@@ -1,3 +1,9 @@
-export default function () {
+import { setSyntax } from "../actions/documents/code";
+
+export default function (store, doc, syntax) {
+    
+    if (store.getState().document.doc_id == doc) {
+        store.dispatch(setSyntax(syntax));
+    }
     
 }
