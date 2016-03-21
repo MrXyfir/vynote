6bed447d309056e7ee8e85cf6407a844eeef4392
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-03-21 15:47:28 -0700

    Add controller for updating document content

--- client/sockets/update-content.js
+++ client/sockets/update-content.js
@@ -1,3 +1,9 @@
-export default function () {
+import { saveContent } from "../actions/documents/";
+
+export default function (store, data) {
+    
+    if (store.getState().document.doc_id == data.doc) {
+        store.dispatch(saveContent(data.content));
+    }
     
 }
