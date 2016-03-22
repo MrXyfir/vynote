a23966f976bf9c771b28c150ad1ae4fb91cbfc0b
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-03-22 10:47:08 -0700

    Modify to utilize location.hash instead of URL routes

--- client/lib/route/set-route.js
+++ client/lib/route/set-route.js
@@ -1,5 +1,3 @@
-import { URL } from "../../constants/config";
-
 let lastState;
 
 function clean(input) {
@@ -17,25 +15,25 @@ export default function (state) {
         return;
     }
     
-    let route = "", changed = false;
+    let hash = "", changed = false;
     
     // Document has changed
     if (lastState.doc_id != state.doc_id) {
-        route = "workspace/" + state.doc_id + "/" + clean(state.name);
+        hash = state.doc_id + '.' + clean(state.name);
         changed = true;
     }
     // Scope of note document has changed
     else if (state.doc_type == 1 && state.render) {
         if ((!lastState.render && state.render) || (lastState.render.scope != state.render.scope)) {
-            route = "workspace/" + state.doc_id + "/" + clean(state.name)
-                + "/" + state.render.scope + "/"
+            hash = state.doc_id + '.' + clean(state.name)
+                + '/' + state.render.scope + '.'
                 + clean(state.content[state.render.scope].content);
             changed = true;
         }
     }
     
     if (changed) {
-        history.pushState({}, '', URL + route);
+        location.hash = hash;
         lastState = state;
     }
     
