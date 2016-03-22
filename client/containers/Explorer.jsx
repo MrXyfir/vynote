d1c4ba83222feecf2dcc8d71733f21b2cbc49e51
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-03-22 16:43:49 -0700

    Now props.data contains entire state. Add Tabs component. Improve ExplorerObject properties

--- client/containers/Explorer.jsx
+++ client/containers/Explorer.jsx
@@ -1,39 +1,44 @@
 import React from "react";
 
+// Components
 import ExplorerObject from "../components/explorer/ExplorerObject";
 import ControlBar from "../components/explorer/ControlBar";
 import UserInput from "../components/explorer/UserInput";
+import Tabs from "../components/explorer/tabs";
 
 export default class Explorer extends React.Component {
 	
 	render() {
 		return (
 			<div className="explorer">
+                <Tabs
+                    data={this.props.data}
+					socket={this.props.socket}
+					dispatch={this.props.dispatch}
+                />
 				<ControlBar 
-					data={this.props.data} 
-					socket={this.props.socket} 
+					data={this.props.data.explorer}
+					socket={this.props.socket}
 					dispatch={this.props.dispatch}
 				/>
 				<div className="explorer-objects">{
-                    this.props.data.children[this.props.data.scope] !== undefined
-					? this.props.data.children[this.props.data.scope].map(child => {
-						let isDoc = (child.type == 2);
-						
+                    this.props.data.explorer.children[this.props.data.explorer.scope] !== undefined
+					? this.props.data.explorer.children[this.props.data.explorer.scope].map(child => {
 						return (
 							<ExplorerObject
-                                user={this.props.user}
-								data={this.props.data[isDoc ? "documents" : "folders"][child.id]} 
-								socket={this.props.socket} 
-								isDoc={isDoc} 
-                                hover={this.props.data.hover} 
+                                id={child.id}
+                                type={child.type}
+								data={this.props.data}
+                                group={child.type == 1 ? "folders" : "documents"} 
+								socket={this.props.socket}  
 								dispatch={this.props.dispatch}
 							/>
 						);
 					}) : <div />
 				}</div>
 				<UserInput
-                    user={this.props.user}
-					data={this.props.data} 
+                    user={this.props.data.user}
+					data={this.props.data.explorer} 
 					socket={this.props.socket} 
 					dispatch={this.props.dispatch} 
 				/>
