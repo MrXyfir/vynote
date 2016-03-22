2e14d433981cc5b038ca808b938fc0e044073ebe
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-03-22 10:46:24 -0700

    Update to utilize location.hash instead of URL path. Fix navigating to note element

--- client/lib/route/set-state.js
+++ client/lib/route/set-state.js
@@ -1,31 +1,33 @@
 // Action creators
 import { loadDocument } from "../../actions/documents/";
 import {
-initializeRenderObject, navigateToElement
-    
+    initializeRenderObject, navigateToElement
 } from "../../actions/documents/note";
 
 // Modules
+import doesMatch from "./does-match";
 import buildNote from "../../lib/note/build";
 
-export default function (store, socket) {
+export default function (store, socket, check = false) {
     
-    // Parse URL
-    let a = document.createElement('a');
-    a.href = location.href;
-    let paths = a.pathname.split('/').slice(2);
-    
-    if (paths[0] === undefined) return;
+    if (location.hash.length < 2) return;
     
     let state = store.getState();
     
+    // If URL matches state and check == true, return
+    if (check && doesMatch(state)) return;
+    
+    // Get document by id
+    // vynote.com/workspace/#docID.doc-name/noteID.note-content
+    let id  = location.hash.substr(1).split('.')[0];
+    let doc = state.explorer.documents[id];
+    
     // Document does not exist
-    if (state.explorer.documents[paths[0]] === undefined) return;
-    
-    let doc = state.explorer.documents[paths[0]];
-    
-    // Let Document container handle loading encrypted document
-    if (doc.encrypted) {
+    if (doc === undefined) {
+        return;
+    }
+    // Encrypted document
+    else if (doc.encrypted) {
         let data = Object.assign({}, doc, {
             encrypt: "", theme: state.user.config.defaultEditorTheme
         });
@@ -33,37 +35,44 @@ export default function (store, socket) {
         if (doc.doc_type == 2)
             data.preview = state.user.config.defaultPageView == "preview";
         
-        this.props.dispatch(loadDocument(data));
+        store.dispatch(loadDocument(data));
     }
     // Note document
     else if (doc.doc_type == 1) {
-        socket.emit("get note object", paths[0], "", (err, res) => {
+        socket.emit("get note object", id, "", (err, res) => {
             if (!err) {
+                let element = "";
+                
+                // Grab note element (if available) before we load document
+                if (location.hash.split('/')[1] !== undefined)
+                    element = location.hash.split('/')[1].split('.')[0];
+                
+                let content = buildNote(res.content, res.changes);
+                
+                // Load document and initialize render
                 store.dispatch(loadDocument(
-                    Object.assign({}, this.props.data, {
-                        content: buildNote(res.content, res.changes)
-                    })
+                    Object.assign({}, doc, { content })
                 ));
                 store.dispatch(initializeRenderObject());
                 
-                if (paths[3] !== undefined) {
-                    store.dispatch(navigateToElement(paths[3]));
-                }
+                // Navigate to element present in previous url
+                if (content[element] !== undefined)
+                    store.dispatch(navigateToElement(element));
             }
         });
     }
     // Other document
     else {
-        socket.emit("get document content", paths[0], "", (err, res) => {
+        socket.emit("get document content", id, "", (err, res) => {
             if (!err) {
                 let data = Object.assign({}, doc, {
-                    content: res, theme: this.props.user.config.defaultEditorTheme
+                    content: res, theme: state.user.config.defaultEditorTheme
                 });
                 
                 if (doc.doc_type == 2)
-                    data.preview = this.props.user.config.defaultPageView == "preview";
+                    data.preview = state.user.config.defaultPageView == "preview";
                 
-                this.props.dispatch(loadDocument(data));
+                store.dispatch(loadDocument(data));
             }
         });
     }
