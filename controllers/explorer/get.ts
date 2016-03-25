91f6037415c27dab937bac8a2745a907a8dcb1db
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-03-24 18:53:26 -0700

    Select folder/document color

--- controllers/explorer/get.ts
+++ controllers/explorer/get.ts
@@ -5,7 +5,7 @@ export = (socket: SocketIO.Socket, fn: Function) => {
     db(cn => {
         // Grab all of user's folders
         let sql: string = `
-            SELECT parent_id, folder_id, name FROM folders WHERE user_id = ?
+            SELECT parent_id, folder_id, name, color FROM folders WHERE user_id = ?
         `;
 
         cn.query(sql, [socket.session.uid], (err, rows) => {
@@ -23,7 +23,7 @@ export = (socket: SocketIO.Socket, fn: Function) => {
             // Grab info for all of user's documents
             sql = `
                 SELECT doc_type, doc_id, folder_id, name, IF(encrypt != '', 1, 0) as encrypted, 
-                created, syntax, 0 as contributor FROM documents WHERE user_id = ?
+                created, syntax, color, 0 as contributor FROM documents WHERE user_id = ?
             `;
 
             cn.query(sql, [socket.session.uid], (err, rows) => {
@@ -32,7 +32,7 @@ export = (socket: SocketIO.Socket, fn: Function) => {
                 // Grab documents that user is a contributor on
                 sql = `
                     SELECT documents.doc_type, documents.doc_id, documents.name, IF(documents.encrypt != '', 1, 0) as encrypted, 
-                    documents.created, documents.syntax, document_contributors.folder_id, 1 as contributor 
+                    documents.created, documents.syntax, documents.color, document_contributors.folder_id, 1 as contributor 
                     FROM documents, document_contributors 
                     WHERE documents.doc_id IN (
                         SELECT document_contributors.doc_id FROM document_contributors WHERE user_id = 1
