1875ff4c88c9f3dc7f8fc35b6c6e54cab09f15f4
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-03-30 15:35:44 -0700

    Add module to determine changes in text document

---
+++ client/lib/document/diff.js
@@ -0,0 +1,27 @@
+import jsdiff from "diff";
+
+export default function (oldText, newText) {
+    
+    let index = 0, diff = [];
+
+    jsdiff.diffChars(oldText, newText).forEach(change => {
+        if (change.added) {
+            diff.push({
+                insert: change.value, at: index
+            });
+            
+            index += change.count;
+        }
+        else if (change.removed) {
+            diff.push({
+                remove: true, at: index, for: change.count
+            });
+        }
+        else {
+            index += change.count;
+        }
+    });
+
+    return diff;
+  
+}
