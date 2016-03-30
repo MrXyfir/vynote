d16a2c94eabee7269c352510082c9939366f9f3f
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-03-29 17:41:03 -0700

    Mark document for reload

--- client/sockets/update-content.js
+++ client/sockets/update-content.js
@@ -1,4 +1,4 @@
-import { saveContent } from "../actions/documents/";
+import { saveContent, toggleMarkForReload } from "../actions/documents/";
 import { markForReload } from "../actions/explorer/tabs";
 
 export default function (store, data) {
@@ -7,6 +7,7 @@ export default function (store, data) {
     
     if (state.document.doc_id == data.doc) {
         store.dispatch(saveContent(data.content));
+        store.dispatch(toggleMarkForReload());
     }
     else if (data.doc in state.explorer.tabs.list) {
         store.dispatch(markForReload(data.doc));
