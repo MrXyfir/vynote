e579d9fa39747af2558900571e7a0d215baf760c
theycallmexshadow <theycallmexshadow@bitbucket.org>
2016-03-30 15:11:50 -0700

    Add module for inserting/removing to/from a document based on changes

---
+++ lib/document/update.ts
@@ -0,0 +1,21 @@
+﻿interface IChanges {
+    insert?: string, remove?: boolean, at: number, for?: number
+}
+
+function update(content: string, changes: IChanges[]) {
+    changes.forEach(change => {
+        if (change.insert) {
+            content = content.substring(0, change.at)
+                + change.insert
+                + content.substring(change.at);
+        }
+        else {
+            content = content.substring(0, change.at)
+                + content.substring(change.at + change.for);
+        }
+    });
+
+    return content;
+}
+
+export = update;
