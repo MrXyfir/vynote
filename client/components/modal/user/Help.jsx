a79f0fefa0d9e74469702c9472936359e418f86c
MrXyfir <mr@xyfir.com>
2016-08-31 10:30:45 -0700

    Update to pull docs from GitHub/Xyfir/Documentation

--- client/components/modal/user/Help.jsx
+++ client/components/modal/user/Help.jsx
@@ -1,24 +1,44 @@
 import marked from "marked";
 import React from "react";
 
-// Action creators
+// Actions
 import { error, success } from "actions/notification";
 
 // Constants
 import { URL } from "constants/config";
 
+// Components
+import DynamicIframe from "components/contained/misc/DynamicIframe";
+
+// Modules
+import request from "lib/request";
+
 export default class Help extends React.Component {
-	
+
     componentDidMount() {
-        let http = new XMLHttpRequest();
-        http.onreadystatechange = () => { 
-            document.querySelector(".user-help").innerHTML = marked(http.responseText);
-        }
-        http.open("GET", URL + "documentation/help.md", true); http.send();
+        const url = "https://api.github.com/repos/Xyfir/Documentation/contents"
+            + "/vynote/help.md";
+
+        request({url, success: (res) => {
+            // Add CSS files
+            this.refs.frame.refs.frame.contentDocument.head.innerHTML = `
+                <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,700" rel="stylesheet" type="text/css">
+                <link rel="stylesheet" href="static/css/style.css">
+            `;
+
+            // Convert markdown to html
+            this.refs.frame.refs.frame.contentDocument.body.innerHTML = `
+                <div class="markdown">${
+                    marked(
+                        window.atob(res.content), { santize: true }
+                    )
+                }</div>
+            `;
+        }});
     }
     
 	render() {
-		return <div className="user-help markdown" />;
+		return <DynamicIframe ref="frame" className="user-help" />
 	}
 
 }
