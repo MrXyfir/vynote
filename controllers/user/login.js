705fb39a3fc965ec47646723d37062c86dc5c756
MrXyfir <mr@xyfir.com>
2016-09-21 14:56:40 -0700

    Generate and save an access code on successful login

--- controllers/user/login.js
+++ controllers/user/login.js
@@ -1,4 +1,5 @@
-﻿const request = require("request");
+﻿const rstring = require("randomstring");
+const request = require("request");
 const session = require("lib/session");
 const db = require("lib/db");
 
@@ -10,6 +11,7 @@ module.exports = function(socket, xid, auth, fn) {
         + config.keys.xacc + "/" + xid + "/" + auth;
 
     request(url, (err, response, body) => {
+        // Error in request
         if (err) {
             fn(true);
             return;
@@ -17,55 +19,68 @@ module.exports = function(socket, xid, auth, fn) {
 
         body = JSON.parse(body);
 
+        // Error from Xyfir Accounts
         if (body.error) {
             fn(true);
             return;
         }
 
-        db(cn => {
-            let sql = "SELECT user_id, subscription FROM users WHERE xyfir_id = ?";
-            cn.query(sql, [xid], (err, rows) => {
+        let sql = "SELECT user_id, subscription FROM users WHERE xyfir_id = ?";
 
-                // First login
-                if (rows.length == 0) {
-                    let insert = {
-                        xyfir_id: xid, email: body.email
-                    };
-                    sql = "INSERT INTO users SET ?";
+        db(cn => cn.query(sql, [xid], (err, rows) => {
+            // Generates a 3-day access code for user
+            const generateAccessCode = () => {
+                const code = rstring.generate(64);
 
-                    cn.query(sql, insert, (err, result) => {
+                sql = `
+                    INSERT INTO access_codes (code, expires, user_id)
+                    VALUES (?, DATE_ADD(NOW(), INTERVAL 3 DAY), ?)
+                `;
+                cn.query(sql, [code, rows[0].user_id], (err, result) => {
+                    cn.release();
+
+                    if (err || !result.affectedRows)
+                        fn(false, ""); // will fail 'get user info' and force login
+                    else
+                        fn(false, rows[0].user_id + "-" + code);
+                });
+            };
+
+            // First login
+            if (rows.length == 0) {
+                let insert = {
+                    xyfir_id: xid, email: body.email
+                };
+                sql = "INSERT INTO users SET ?";
+
+                cn.query(sql, insert, (err, result) => {
+                    if (err || !result.affectedRows) {
                         cn.release();
-
-                        if (err || !result.affectedRows) {
-                            fn(true);
-                        }
-                        else {
-                            session.save(socket.id, { uid: result.insertId, subscription: 0 });
-                            fn(false);
-                        }
-                    });
-                }
-                // Update data
-                else {
-                    sql = "UPDATE users SET email = ? WHERE user_id = ?";
-                    cn.query(sql, [body.email, rows[0].user_id], (err, result) => {
+                        fn(true);
+                    }
+                    else {
+                        session.save(socket.id, { uid: result.insertId, subscription: 0 });
+                        generateAccessCode();
+                    }
+                });
+            }
+            // Update data
+            else {
+                sql = "UPDATE users SET email = ? WHERE user_id = ?";
+                cn.query(sql, [body.email, rows[0].user_id], (err, result) => {
+                    if (err) {
                         cn.release();
-
-                        if (err) {
-                            fn(true);
-                        }
-                        else {
-                            session.save(socket.id, {
-                                uid: rows[0].user_id, subscription: rows[0].subscription
-                            });
-
-                            fn(false);
-                        }
-                    });
-                }
-
-            });
-        });
+                        fn(true);
+                    }
+                    else {
+                        session.save(socket.id, {
+                            uid: rows[0].user_id, subscription: rows[0].subscription
+                        });
+                        generateAccessCode();
+                    }
+                });
+            }
+        }));
     });
 
 }
