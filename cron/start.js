2f0cded3e7091a27fb56df0c36b1fa4c16c67846
MrXyfir <mr@xyfir.com>
2016-10-08 08:59:29 -0700

    Remove 'delete expired access codes' cron job

--- cron/start.js
+++ cron/start.js
@@ -7,8 +7,7 @@ const cron = require("cron");
 module.exports = function() {
     
     const jobs = {
-        deleteExpiredDocumentVersions: require("./delete-expired-document-versions"),
-        deleteExpiredAccessCodes: require("./delete-expired-access-codes")
+        deleteExpiredDocumentVersions: require("./delete-expired-document-versions")
     };
 
     // Delete document versions over a week old
@@ -16,13 +15,6 @@ module.exports = function() {
     // Retries once on failure
     new cron.CronJob("0 1 * * *", () => jobs.deleteExpiredDocumentVersions(err => {
         if (err) jobs.deleteExpiredDocumentVersions(err => { return; });
-    }), () => { return; }, true);
-
-    // Delete access codes over 3 days old
-    // Runs every 6th hour
-    // Retries once on failure
-    new cron.CronJob("0 1/6 * * *", () => jobs.deleteExpiredAccessCodes(err => {
-        if (err) jobs.deleteExpiredAccessCodes(err => { return; });
     }), () => { return; }, true);
 
 };
